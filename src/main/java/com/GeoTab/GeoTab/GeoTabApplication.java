4761255b62a4499185770ff3ff4f481dbbac6012
hover10tsuna@outlook.com <hover10tsuna@outlook.com>
2024-12-05 16:54:25 -0500

    no message

---
+++ src/main/java/com/GeoTab/GeoTab/GeoTabApplication.java
@@ -0,0 +1,13 @@
+package com.GeoTab.GeoTab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoTabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoTabApplication.class, args);
+	}
+
+}
